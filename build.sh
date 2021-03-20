c6ddd8913010ce1d1d00b35ebf6e92ae0a9321d1
Damien Eversmann <damien.eversmann@gmail.com>
2021-03-19 22:31:33 -0500

    Cleaned up the build script and automated more

--- build.sh
+++ build.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
+RHEL_ISO="/Users/deversma/Downloads/isos/rhel-8.3-x86_64-dvd.iso"
 VM="rhelbox-$(date +%s)"
-VBoxManage createvm -name $VM --ostype RedHat_64 --default --register --basefolder "$(realpath $(dirname ${BASH_SOURCE:-0}))"
+BUILD_DIR="build"
+RELEASE_DIR="release"
+
+# sanity check
+[[ ! -e $RHEL_ISO ]] && { echo "Does not exist: $RHEL_ISO"; exit 9999; }
+[[ ! -x "$(command -v VBoxManage)" ]] && { echo "Script requires VirtualBox CLI"; exit 9999; }
+[[ ! -x "$(command -v vagrant)" ]] && { echo "Script requires Vagrant"; exit 9999; }
+
+# clean
+[[ -e $BUILD_DIR/.metadata ]] && VBoxManage unregistervm $(head -1 $BUILD_DIR/.metadata) --delete
+[[ -e $BUILD_DIR ]] && rm -rf $BUILD_DIR
+
+# setup
+mkdir -p $BUILD_DIR
+echo $VM > $BUILD_DIR/.metadata
+
+# build kickstart disk image
+mkdir $BUILD_DIR/oemdrv
+cp ks.cfg $BUILD_DIR/oemdrv/ks.cfg
+hdiutil create -format UDTO -fs MS-DOS -volname OEMDRV -srcfolder $BUILD_DIR/oemdrv $BUILD_DIR/oemdrv.cdr
+
+# build VM
+VBoxManage createvm -name $VM --ostype RedHat_64 --default --register --basefolder $BUILD_DIR
 VBoxManage modifyvm $VM --cpus 2 --memory 2048
-VBoxManage createhd --filename $VM/$VM.vdi --size 20480
-VBoxManage storageattach $VM --storagectl "SATA" --port 0 --device 0 --type hdd --medium $VM/$VM.vdi
-VBoxManage clonemedium disk rhelbox_ks.vdi $VM/rhelbox_ks.vdi
-VBoxManage storageattach $VM --storagectl "SATA" --port 1 --device 0 --type hdd --medium $VM/rhelbox_ks.vdi
-VBoxManage storageattach $VM --storagectl "IDE" --port 0 --device 0 --type dvddrive --medium /Users/deversma/Downloads/isos/rhel-8.3-x86_64-dvd.iso
-VBoxManage storageattach $VM --storagectl "IDE" --port 0 --device 1 --type dvddrive --medium /Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso
+VBoxManage createhd --filename $BUILD_DIR/$VM/$VM.vdi --size 20480
+VBoxManage storageattach $VM --storagectl "SATA" --port 0 --device 0 --type hdd --medium $BUILD_DIR/$VM/$VM.vdi
+VBoxManage convertdd $BUILD_DIR/oemdrv.cdr $BUILD_DIR/$VM/oemdrv.vdi
+VBoxManage storageattach $VM --storagectl "SATA" --port 1 --device 0 --type hdd --medium $BUILD_DIR/$VM/oemdrv.vdi
+VBoxManage storageattach $VM --storagectl "IDE" --port 0 --device 0 --type dvddrive --medium $RHEL_ISO
+VBoxManage storageattach $VM --storagectl "IDE" --port 0 --device 1 --type dvddrive --medium emptydrive # https://www.virtualbox.org/ticket/13040
+VBoxManage storageattach $VM --storagectl "IDE" --port 0 --device 1 --type dvddrive --medium additions
+
+# boot VM and wait for shutdown after kickstart install
 VBoxManage startvm $VM --type headless
 until $(VBoxManage showvminfo --machinereadable $VM | grep -q ^VMState=.poweroff.)
 do
@@ -16,6 +42,13 @@ do
   sleep 10
 done
 echo "Done"
+
+# remove extra media from VM
 VBoxManage storageattach $VM --storagectl "IDE" --port 0 --device 1 --medium none
 VBoxManage storageattach $VM --storagectl "SATA" --port 1 --device 0 --medium none
-VBoxManage closemedium disk $VM/rhelbox_ks.vdi --delete
+VBoxManage closemedium disk $BUILD_DIR/$VM/oemdrv.vdi --delete
+
+# package VM as vagrant box
+[[ ! -d $RELEASE_DIR ]] && mkdir -p $RELEASE_DIR
+vagrant package --base $VM --output $RELEASE_DIR/$VM.box
+echo "SHA1: $( sha1sum $RELEASE_DIR/$VM.box )"
